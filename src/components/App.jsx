de1021c3d0e6172b9383a9b471a9ee6419263e7a
arapozo <rapozoantonio@gmail.com>
2020-11-26 13:30:13 -0300

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "./Card.jsx";
+import emojipedia from "../emojipedia.js";
+
+
+function createCard(props) {
+  return(
+      <Card 
+            key={props.id}
+            id={props.id}
+            emoji={props.emoji}
+            name={props.name}
+            meaning={props.meaning}
+      />
+  );
+};
+
+function App(props) {
+  return (
+    <div>
+      <h1>
+        <span>Antonio's most used emojis</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map(createCard)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,62 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning:
+      "Often used as a playful way to convey a laughing, disbelieving, or cringing I can’t believe what I’m seeing! or I can’t bear to look!"
+  },
+  {
+    id: 2,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:
+      "May be paired with 💭 Thought Balloon or 💡 Light Bulb to represent thinking, (having) ideas, and learning."
+  },
+  {
+    id: 3,
+    emoji: "😈",
+    name: "Smiling Face with Horns",
+    meaning:
+      "Commonly used to convey mischief, naughtiness, and excitement or excellence (slang, bad or wicked)."
+  },
+  {
+    id: 4,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing."
+  },
+  {
+    id: 5,
+    emoji: "😜",
+    name: "Winking Face with Tongue",
+    meaning:
+      " Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking."
+  },
+  {
+    id: 6,
+    emoji: "🙏",
+    name: "Folded Hands",
+    meaning:
+      "A common alternative use for this emoji is for prayer, using the same gesture as praying hands. Rarely: a high-five."
+  },
+  {
+    id: 7,
+    emoji: "💩",
+    name: "Pile of Poo",
+    meaning:
+      "A swirl of brown poop, shaped like soft-serve ice cream with large, excited eyes and a big, friendly smile"
+  },
+  {
+    id: 8,
+    emoji: "✔️",
+    name: "Check Mark",
+    meaning:
+      "Commonly used in lists to signify completed tasks or verification of a statement of fact."
+  },    
+];
+
+
+
+export default emojipedia;
